599ebeb603808410fe7cc65842b14f782190f527
NinaBalan <52449291+NinaBalan@users.noreply.github.com>
2020-02-27 12:25:36 +0200

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,12 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 
-gulp.task('serve',function(){
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-    gulp.watch("*.html").on("change",reload);
+gulp.task('serve',function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+
+  gulp.watch("*.html").on("change",reload);
 });
